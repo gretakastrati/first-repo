24f2d13391270f0885509fca368f21e069107e9d
pitagoras <kastratigreta1@gmail.com>
2023-04-09 16:41:11 +0200

    Test

--- First.java
+++ First.java
@@ -2,6 +2,7 @@ public class First {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello") ;// conflict test
+        System.out.println("Hello") ;// test conflict 
+        System.out.println("Test");
     }
 }
